a7558d45ff9d378ba489591fc0282aaee9b2d7aa
MB <mendelbak@gmail.com>
2020-10-08 04:14:08 +0300

    Broke Table out of App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, { Component, useState } from 'react';
 import styled from 'styled-components';
-import { useTable } from 'react-table';
 import SlidingSidebar from './SlidingSidebar';
-import Card from './Card';
-
-import { Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react';
+import Table from './Table';
 
 import makeData from './makeData';
 
@@ -43,106 +40,6 @@ const Styles = styled.div`
     }
   }
 `;
-
-// Create an editable cell renderer
-const EditableCell = ({
-  value: initialValue,
-  row: { index },
-  column: { id },
-}) => {
-  const [value, setValue] = React.useState(initialValue);
-
-  const onChange = (e) => {
-    setValue(e.target.value);
-  };
-
-  function clearInputValue() {
-    setValue('');
-  }
-
-  const [sidebarVisible, setSidebarVisible] = React.useState(false);
-
-  function openSlidingSidebar() {
-    console.log('asdfjlhaslkdj');
-    setSidebarVisible(true);
-  }
-
-  return (
-    <div className='ui transparent fluid left icon input '>
-      <i
-        className='expand arrows alternate link icon'
-        onClick={openSlidingSidebar}
-      ></i>
-      <input
-        type='text'
-        value={value}
-        onChange={onChange}
-        style={{ maxWidth: '90%' }}
-      />
-      <i
-        className='delete link icon'
-        style={{ left: 'auto', right: '1px' }}
-        onClick={clearInputValue}
-      ></i>
-    </div>
-  );
-};
-
-// Set our editable cell renderer as the default Cell renderer
-const defaultColumn = {
-  Cell: EditableCell,
-};
-
-function Table({ columns, data, updateMyData, skipPageReset }) {
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    prepareRow,
-    rows,
-  } = useTable({
-    columns,
-    data,
-    defaultColumn,
-  });
-
-
-  return (
-    <>
-      <Card />
-
-      <div className='ui basic center aligned segment'>
-        <div className='ui horizontal divider'>Data Table</div>
-      </div>
-
-      <table {...getTableProps()} className='ui celled table'>
-        <thead>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row, i) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => {
-                  return (
-                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                  );
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </>
-  );
-}
 
 function App() {
   const columns = React.useMemo(
@@ -196,7 +93,7 @@ function App() {
       </div>
 
       <SlidingSidebar
-        visible={true}
+        visible={false}
         direction={'right'}
         width={'very wide'}
         // animation={'scale down'}
--- src/Table.js
+++ src/Table.js
@@ -1,42 +1,102 @@
 import React from 'react';
 import { useTable } from 'react-table';
+// import EditableCell from './EditableCell';
+import Card from './Card';
 
-export default function Table({ columns, data }) {
-  // Use the state and functions returned from useTable to build your UI
+const EditableCell = ({
+  value: initialValue,
+  row: { index },
+  column: { id },
+}) => {
+  const [value, setValue] = React.useState(initialValue);
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  function clearInputValue() {
+    setValue('');
+  }
+
+  // const [sidebarVisible, setSidebarVisible] = React.useState(false);
+
+  // function openSlidingSidebar() {
+  //   console.log('asdfjlhaslkdj');
+  //   setSidebarVisible(true);
+  // }
+
+  return (
+    <div className='ui transparent fluid left icon input '>
+      <i
+        className='expand arrows alternate link icon'
+        // onClick={openSlidingSidebar}
+      ></i>
+      <input
+        type='text'
+        value={value}
+        onChange={onChange}
+        style={{ maxWidth: '90%' }}
+      />
+      <i
+        className='delete link icon'
+        style={{ left: 'auto', right: '1px' }}
+        onClick={clearInputValue}
+      ></i>
+    </div>
+  );
+};
+
+// Set our editable cell renderer as the default Cell renderer
+const defaultColumn = {
+  Cell: EditableCell,
+};
+
+export default function Table({ columns, data, updateMyData, skipPageReset }) {
   const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    rows,
     prepareRow,
+    rows,
   } = useTable({
     columns,
     data,
+    defaultColumn,
   });
 
   return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
-              })}
+    <>
+      <Card />
+
+      <div className='ui basic center aligned segment'>
+        <div className='ui horizontal divider'>Data Table</div>
+      </div>
+
+      <table {...getTableProps()} className='ui celled table'>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
   );
 }
